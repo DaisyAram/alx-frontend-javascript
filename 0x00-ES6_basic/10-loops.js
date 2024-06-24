bb113e45dc70312782f8e16e0f4365bab0b2632f
DaisyAram <daisyarama03@gmail.com>
2024-06-24 11:43:59 +0000

    for/of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = array;
   for (const value of array) {
-    const idx =  array.indexOf(value);
+    const idx = array.indexOf(value);
     newArray[idx] = appendString + value;
   }
 
