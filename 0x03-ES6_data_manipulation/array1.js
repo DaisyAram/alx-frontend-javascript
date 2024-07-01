adb7ae438c4463f3cdb00d94562e2ca75d1003c2
DaisyAram <daisyarama03@gmail.com>
2024-07-01 17:16:23 +0000

    2. Filter

--- 0x03-ES6_data_manipulation/array1.js
+++ 0x03-ES6_data_manipulation/array1.js
@@ -1,3 +1,17 @@
 const numbers = [1, 2, 3, 4, 5];
 const sum = numbers.reduce((acc, current) => acc + current, 0);
 console.log(sum);
+
+/*const colors = ["red", "orange", "yellow", "green"];
+colors[5] = "blue";
+const iterator = colors.keys();
+for (const key of iterator) {
+	console.log(`${key}: ${colors[key]}`);
+
+}*/
+
+const colors = ["red", "yellow", "blue"];
+colors[5] = "purple";
+colors.forEach((item, index) => {
+  console.log(`${index}: ${item}`);
+});
