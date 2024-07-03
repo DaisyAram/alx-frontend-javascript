337394f4849282130315e0c5d149da68593f0211
DaisyAram <daisyarama03@gmail.com>
2024-07-03 14:09:11 +0000

    export

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,5 +1,5 @@
 //building teacher interface
-interface Teacher {
+export interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
@@ -44,4 +44,3 @@ class StudentClass implements classInterface {
   }
 
   };
-export { printTeacher, StudentClass };
