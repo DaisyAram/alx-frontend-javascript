d9eb5975709ee48f902a61e63b0add83fe5f8e81
DaisyAram <daisyarama03@gmail.com>
2024-07-03 12:37:05 +0000

    printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+//building teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -6,3 +7,16 @@ interface Teacher {
   location: string;
   [key: string]: any
 }
+
+//extending the teacher class
+interface Directors extends Teacher {
+  numberofReports: number;
+}
+
+//printing teachers
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string
+  }
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+return `${firstName[0]}.${lastName}`;
+};
